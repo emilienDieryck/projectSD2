32629bbe81c027147342a788011a0f3e671e4534
paul.vandecasteele <paul.vandecasteele@student.vinci.be>
2024-03-01 14:39:13 +0100

    init

---
+++ src/Graph.java
@@ -0,0 +1,8 @@
+public class Graph {
+
+  public void calculerItineraireMinimisantNombreRoutes(String berlin, String madrid) {
+  }
+
+  public void calculerItineraireMinimisantKm(String berlin, String madrid) {
+  }
+}
