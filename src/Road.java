7ba7a78dc140a5035f799ece8de8716434e99d62
Cwen28 <94372138+Cwen28@users.noreply.github.com>
2024-03-01 15:39:28 +0100

    add : class road

---
+++ src/Road.java
@@ -0,0 +1,44 @@
+import java.util.Objects;
+
+public class Road {
+  private final int extremite1;
+  private final int extremite2;
+
+  public Road (int extremite1, int extremite2) {
+    this.extremite1 = extremite1;
+    this.extremite2 = extremite2;
+  }
+
+  public int getExtremite1() {
+    return extremite1;
+  }
+
+  public int getExtremite2() {
+    return extremite2;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    Road road = (Road) o;
+    return extremite1 == road.extremite1 && extremite2 == road.extremite2;
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(extremite1, extremite2);
+  }
+
+  @Override
+  public String toString() {
+    return "Road{" +
+        "extremite1=" + extremite1 +
+        ", extremite2=" + extremite2 +
+        '}';
+  }
+}
